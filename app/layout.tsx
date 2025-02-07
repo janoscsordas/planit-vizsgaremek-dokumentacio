1c830f46a3aa7a7618e6ddb4bd780e40f91cb057
Koós Balázs Bence <koosbalazsbence@gmail.com>
2025-02-07 12:25:12 +0100

    Change overview for developer page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,42 @@
-import { Footer, LastUpdated, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head, Search } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
- 
+import { Footer, LastUpdated, Layout, Navbar } from "nextra-theme-docs"
+import { Banner, Head, Search } from "nextra/components"
+import { getPageMap } from "nextra/page-map"
+import "nextra-theme-docs/style.css"
+
 export const metadata = {
   title: "Planitapp Dokumentáció",
   description: "Planitapp alkalmazás teljeskörű dokumentációja.",
 }
- 
-const banner = <Banner storageKey="some-key">Megérkezett a Planitapp dokumentációja. 🎉</Banner>
+
+const banner = (
+  <Banner storageKey="some-key">
+    Megérkezett a Planitapp dokumentációja. 🎉
+  </Banner>
+)
 const navbar = (
   <Navbar
     logo={<b>Planitapp</b>}
-    projectLink='https://github.com/janoscsordas/planit-vizsgaremek-fullstack'
+    projectLink="https://github.com/janoscsordas/planit-vizsgaremek-fullstack"
   />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Planitapp. Minden jog fenntartva.</Footer>
- 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+const footer = (
+  <Footer>
+    MIT {new Date().getFullYear()} © Planitapp. Minden jog fenntartva.
+  </Footer>
+)
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html
-      lang="hu"
-      dir="ltr"
-      suppressHydrationWarning
-    >
+    <html lang="hu" dir="ltr" suppressHydrationWarning>
       <Head
-      color={{
-        hue: 130
-      }}
-      >
-      </Head>
+        color={{
+          hue: 130,
+        }}
+      ></Head>
       <body>
         <Layout
           banner={banner}
@@ -40,9 +47,12 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           sidebar={{ defaultMenuCollapseLevel: 1, autoCollapse: true }}
           footer={footer}
           feedback={{ content: "Kérdése van? Küldjön visszajelzést." }}
-          lastUpdated={<LastUpdated locale='hu'>Utoljára frissítve: </LastUpdated>}
-          search={<Search placeholder='Keresés...' />}
-          themeSwitch={{ dark: 'Sötét', light: 'Világos', system: 'Rendszer' }}
+          lastUpdated={
+            <LastUpdated locale="hu">Utoljára frissítve: </LastUpdated>
+          }
+          search={<Search placeholder="Keresés..." />}
+          themeSwitch={{ dark: "Sötét", light: "Világos", system: "Rendszer" }}
+          toc={{ title: "Tartalomjegyzék:" }}
         >
           {children}
         </Layout>
