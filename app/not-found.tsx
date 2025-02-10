7681ea87dd87c01cb99ef5f00202320c799cd194
Koós Balázs Bence <koosbalazsbence@gmail.com>
2025-02-10 14:26:43 +0100

    Refactor packages page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,18 @@
-import Link from "next/link";
-import { NotFoundPage } from "nextra-theme-docs";
+import Link from "next/link"
+import { NotFoundPage } from "nextra-theme-docs"
 
 export default function NotFound() {
-    return (
-        <NotFoundPage content={null}>
-            <h1 className="text-2xl font-bold mb-2">A keresett oldal nem található!</h1>
-            <Link href="/" className="underline">Vissza a főoldalra</Link>
-        </NotFoundPage>
-    )
+  return (
+    <NotFoundPage
+      content={null}
+      className="flex flex-col items-center justify-center min-h-screen"
+    >
+      <h1 className="text-4xl font-bold mb-4">
+        A keresett oldal nem található!
+      </h1>
+      <Link href="/" className="underline">
+        Vissza a főoldalra
+      </Link>
+    </NotFoundPage>
+  )
 }
