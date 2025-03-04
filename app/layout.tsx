4636cb5378ac34713249cadc99e77d2fed72e9ab
Koós Balázs Bence <koosbalazsbence@gmail.com>
2025-03-04 13:27:44 +0100

    Update layout with main logo and also update not-found page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,43 @@
-import { Footer, LastUpdated, Layout, Navbar } from "nextra-theme-docs";
-import { Banner, Head, Search } from "nextra/components";
-import { getPageMap } from "nextra/page-map";
-import "./globals.css";
-import "nextra-theme-docs/style.css";
+import { Footer, LastUpdated, Layout, Navbar } from "nextra-theme-docs"
+import { Banner, Head, Search } from "nextra/components"
+import { getPageMap } from "nextra/page-map"
+import "./globals.css"
+import "nextra-theme-docs/style.css"
+import Image from "next/image"
 
 export const metadata = {
   title: "Planitapp Dokumentáció",
   description: "Planitapp alkalmazás teljeskörű dokumentációja.",
-};
+}
 
 const banner = (
   <Banner storageKey="some-key">
     Megérkezett a Planitapp dokumentációja. 🎉
   </Banner>
-);
+)
 const navbar = (
   <Navbar
-    logo={<b>Planitapp</b>}
+    logo={
+      <Image
+        src="/images/full-logo.png"
+        alt="Planitapp logo"
+        width={150}
+        height={68}
+      ></Image>
+    }
     projectLink="https://github.com/janoscsordas/planit-vizsgaremek-fullstack"
   />
-);
+)
 const footer = (
   <Footer>
     MIT {new Date().getFullYear()} © Planitapp. Minden jog fenntartva.
   </Footer>
-);
+)
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="hu" dir="ltr" suppressHydrationWarning>
@@ -66,5 +74,5 @@ export default async function RootLayout({
         </Layout>
       </body>
     </html>
-  );
+  )
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,13 +5,24 @@ export default function NotFound() {
   return (
     <NotFoundPage
       content={null}
-      className="flex flex-col items-center justify-center min-h-screen"
+      className="flex flex-col items-center justify-center w-full min-h-screen gap-8"
     >
-      <h1 className="text-4xl font-bold mb-4">
-        A keresett oldal nem található!
-      </h1>
-      <Link href="/" className="underline">
-        Vissza a főoldalra
+      <div className="relative flex flex-col items-center">
+        <h1 className="text-6xl font-bold text-center text-[#44ca85]">404</h1>
+        <h2 className="mt-2 text-2xl font-semibold">Nincs találat!</h2>
+        <p className="max-w-md mt-4 text-base text-center">
+          A keresett oldal nem található. Ellenőrízd a megadott címet, vagy térj
+          vissza a főoldalra.
+        </p>
+      </div>
+
+      <Link
+        href="/"
+        className="px-6 py-3 text-sm font-semibold text-black text-center rounded-md shadow-sm bg-[#44ca85]"
+        aria-label="Vissza a főoldalra"
+      >
+        <span className="relative z-10">Vissza a főoldalra</span>
+        <div className="absolute inset-0 transition-transform duration-300 ease-in-out -translate-x-full group-hover:translate-x-0" />
       </Link>
     </NotFoundPage>
   )
